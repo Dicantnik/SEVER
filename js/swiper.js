dfa96368ca31d922d46ef91dc2925137f2a35a3a
Andry Zubov <alex470158@gmail.com>
2023-04-03 09:49:01 +0300

    added swiper

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+    const swiper = new Swiper('.swiper', {
+        spaceBetween: 30,
+        slidesPerView: 1,
+        
+      /*pagination: {
+        el: '.reviews-dots',
+        bulletActiveClass: 'reviews-dot-active',
+        bulletClass: 'reviews-dot',
+        clickable: true
+      },*/
+
+      navigation: {
+        nextEl: '.right-swipe',
+        prevEl: '.left-swipe',
+      },
+        breakpoints: {
+
+            0: {
+            slidesPerView: 1,
+            centeredSlides: true,
+            spaceBetween: 50,
+            },
+
+            1440: {
+            centeredSlides: false,
+            spaceBetween: 30,
+            slidesPerView: 4,
+            },
+        }
+  
+    });
--- js/trancenttext.js
+++
@@ -1,7 +0,0 @@
-const text = document.querySelectorAll('.product-name')
-text.forEach((item) => {
-    if (window.screen.width <= 500 && item.innerHTML.length > 33){
-        item.innerHTML = item.innerHTML.slice(0,33) + '...'
-
-    }
-})
